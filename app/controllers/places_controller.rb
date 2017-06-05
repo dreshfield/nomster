23caa7012eac1500a70c7a5db4bee30080e2b858
David Dreshfield <david@dreshfield.com>
2017-06-05 02:34:03 +0000

    Fixed place list ordering issue

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,7 +2,7 @@ class PlacesController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
   def index
-    @places = Place.paginate(:page => params[:page], :per_page => 10)
+    @places = Place.paginate(:page => params[:page], :per_page => 10).order('created_at ASC')
   end
 
   def new
